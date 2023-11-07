f660ab1348f754a6ed424b3049909942cf0191bd
maurinl26 <maurin.loic.ac@gmail.com>
2023-11-07 21:50:39 +0000

    tests folder added to validate stencils compilation

---
+++ tests/stencils/test.py
@@ -0,0 +1,8 @@
+from phyex_gt4py.stencils.condensation import condensation
+from phyex_gt4py.stencils.ice_adjust import ice_adjust, iteration
+
+from gt4py.storage import zeros
+condensation_inputs =  [zeros()]
+
+
+
