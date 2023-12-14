260d626cb5fce85b51e8a98e86e3a3da491a855c
Loic Maurin <maurin.loic.ac@gmail.com>
2023-12-14 10:50:19 +0100

    Wrapper test in f2py to call PHYEX fortran functions with python Sur la branche fortran-wrapper Modifications qui seront validées : 	nouveau fichier : src/wrapper/__init__.py 	nouveau fichier : src/wrapper/call_get_data_ice_adjust.py 	nouveau fichier : src/wrapper/getdata_ice_adjust_mod.pyf

---
+++ src/wrapper/call_get_data_ice_adjust.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import getdata_ice_adjust_mod
+
+
+if __name__ == "__main__":
+
+    nproma = 1
+    ngpblks = 1
+    nflevg = 1
+
+    getdata_ice_adjust_mod.getdata_ice_adjust(nproma, ngpblks, nflevg)
